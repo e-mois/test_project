cd2294a802f9beab621f181f44492768d16c0eb9
Eugene Moiseev <eugen.moiseev@gmail.com>
2020-06-30 01:36:39 +0300

    Added list of comments with post ID

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,21 @@
-from flask import Flask
-from app.get_list_posts import make_list_post
+from flask import Flask, abort
+from app.get_list_posts import make_list_post, make_list_comments
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     list_posts = make_list_post('app/post.json', 'app/comments.json')
-    return list_posts
+    return {'posts': list_posts['posts'], 'posts_count': list_posts['posts_count']}
 
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    list_comments = make_list_comments('app/post.json', 'app/comments.json', post_id)
+    if list_comments:
+        return list_comments
+    else:
+        abort(404, description="Resource not found")
 
 if __name__ == "__main__":
     app.run()
--- app/get_list_posts.py
+++ app/get_list_posts.py
@@ -1,8 +1,8 @@
 import json
 import datetime
 
-#filename_posts = 'post.json'
-#filename_comments = 'comments.json'
+filename_posts = 'post.json'
+filename_comments = 'comments.json'
 
 
 def load_json(filename):
@@ -31,8 +31,10 @@ def make_list_post(filename_posts, filename_comments):
 
         return_posts_dict = {
             'posts': [],
-            'posts_count': 0
+            'posts_count': 0,
+            'list_id': []
         }
+        
 
         for post in post_lists['posts']:
             if post['deleted'] is False and format_date(post['date']) < datetime.datetime.now():
@@ -46,9 +48,10 @@ def make_list_post(filename_posts, filename_comments):
                     }
                 )
                 return_posts_dict['posts_count'] += 1
+                return_posts_dict['list_id'].append(post['id'])
 
         return return_posts_dict
-    
+
     except json.decoder.JSONDecodeError:
         print('Файл пуст')
     except FileNotFoundError:
@@ -56,7 +59,36 @@ def make_list_post(filename_posts, filename_comments):
     except ValueError:
         print('Ошибка')
 
+def make_list_comments(filename_posts, filename_comments, post_id):
+    
+    list_posts = make_list_post(filename_posts, filename_comments)
+    if post_id in list_posts['list_id']:    
+        for post in list_posts['posts']:
+            if post['id'] == post_id:
+                if format_date(post['date']) > datetime.datetime.now():
+                    return False
+                else:
+                    need_post = post
+    else:
+        return False
+
+    comments_lists = load_json(filename_comments)
+    comments = []
+    for comment in comments_lists['comments']:
+        if comment['post_id'] == post_id:
+            comments.append(comment)
+
+    return_post = {
+        "id": post_id, 
+        "title": need_post['title'],
+        "date": need_post['date'], 
+        "body": need_post['body'],
+        "comments": comments, 
+        "comments_count": len(comments)
+    }
+    return return_post
+
 
 '''if __name__ == '__main__':
-    make_list_post(filename_posts, filename_comments)'''
+    make_list_comments(filename_posts, filename_comments, 2)'''
 
